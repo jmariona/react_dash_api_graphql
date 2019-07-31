0d37b71c61c382b8ab7e4a535f1bf89895bcb250
jmariona <jmariona66@gmail.com>
2019-07-30 23:20:26 -0500

    folder base and install dependencies

---
+++ front/src/App.tsx
@@ -0,0 +1,10 @@
+import { Container } from '@material-ui/core';
+import React from 'react';
+
+export const App: React.FC = () => {
+	return (
+		<Container>
+			<h1>Wellcome Dash</h1>
+		</Container>
+	);
+};
